b62279b020b3114b2ddbc6449314cca036057a37
Fauzie <mfauziemihram@gmail.com>
2018-12-01 16:22:00 +0800

    EndofDay5JS

---
+++ Day5/home.js
@@ -0,0 +1,35 @@
+var myName;
+var myAge;
+var myBirthdate;
+
+myName = "Stackup";
+myAge = 23;
+myBirthdate = "20/03/1988";
+
+console.log(myName);
+console.log(myAge);
+console.log(myBirthdate);
+
+var myAddress = "JTC Launchpad"
+
+var myTest;
+console.log(typeof(myTest));
+console.log (typeof(myName));
+console.log(typeof(myAge));
+
+var firstName;
+var lastName;
+var space;
+
+firstName = "Rob";
+lastName = "Martins";
+var space = " ";
+var fullName = firstName + space + lastName;
+
+console.log(fullName);
+
+var qty = 2;
+var total = 200;
+var qtyTotal = qty + total;
+
+console.log(qtyTotal);
